2900c60898b88450620353d70654b907d5ffd9fb
Kimber Ogden <mx.kimber@gmail.com>
2023-03-30 13:45:24 -0500

    created params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,40 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product =  Product.find_by(id: params[:id])
+    # @product =  Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
     render :show
   end
 
+  def create
+
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    render :show
+  end
+
+
 end
+
+
+# @product = Product.new(name: "ThermJac Jacket",
+    # price: 10_000, image_url: "https://preview.redd.it/a-thermal-camouflage-jacket-with-42-graphene-patches-and-v0-tbqkkgofozha1.jpg?width=640&crop=smart&auto=webp&s=b9970bb8b343f64e2b465c7c36e868c8dfcae807",
+    # description: "Therm-Invis Jacket | formable | mini-jack port | HueShift | Sec-ACCEL & Sec-URE chip")
+    # @product.save
+    # render :show
+
+
+
+
+
+
+
+
 # render json: @products This will render the same but with "created_at"
 
   # def synthwear
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
+  post "/products" => "products#create"
 end
 
   # get "/products/:synthwear", to: "products#index"
