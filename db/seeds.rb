091b9bfff5192973d7e98e401274c048b6adc2ae
Kimber Ogden <mx.kimber@gmail.com>
2023-03-30 10:23:05 -0500

    Created and aadded to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,15 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-product = Product.new(name: "Hover Boots", price: 1500, image_url: "https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc8
+product = Product.new(name: "Hover Boots", price: 150_000, image_url: "https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc8
 4-baa3-437e-9518-adb32be77984.s3.amazonawscom%2Fpublic%2Fimages%2F3821c0f6-bd9d-4108-8529-056ef571760d_1000x562.jpeg", description: "A pair of black and chrome boots | lightweight frames, water/sludge resistant | g7X-flight chip | accelerator module V -  made by triple-tech")
+product.save
 
-#   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.new(name: "MorphSuit", price: 200_000, image_url: "https://www.morphsuits.com/media/catalog/product/cache/937f440085cd52c4f06eb785557b6967/m/1/m1_m_p_mpinf_1.jpg", description: "New-gen MorphSuit |  breathable  |  formable to any size  |  easy to program  |  H3 hardware jack ports  |  Sec-URE Chip")
+product.save
+
+biochip_upgrades = Product.new(name: "Opt-Tech Elite 5", price: 50_000, image_url: "https://www.venturacountylasik.com/wp-content/uploads/woman-with-a-bionic-eye.jpg", description: "Opt-Tech Designs v.5 software | thermal upgrade | mini-jack retracer port | HueShift | Sec-ACCEL enhancement")
+product.save
+
+#  Character.create(name: "Luke", movie: movies.first)
