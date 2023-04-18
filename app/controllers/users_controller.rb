9158269152ded432de2ad067ca7feaad5221f200
Kimber Ogden <mx.kimber@gmail.com>
2023-04-18 08:52:44 -0500

    Add the rest of CRUD to User, add user routes, add json/jbuilder for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
   def create
     user = User.new(
       name: params[:name],
@@ -13,5 +23,22 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name] || @user.name
+    @user.email = params[:email] || @user.email
+    @user.password = params[:password] || @user.password
+
+    @user.save
+    render :show
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: {message: "user as been destroyed successfully"}
+  end
+
 
 end
