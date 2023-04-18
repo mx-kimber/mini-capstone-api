59d4637ff400fe3f791844b8667b270debdb1e01
Kimber Ogden <mx.kimber@gmail.com>
2023-04-18 13:29:57 -0500

    Fix create action in orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,17 +17,17 @@ class OrdersController < ApplicationController
 
   def create
     product = Product.find_by(id: params[:product_id])
-    calc_subtotal = params[:quantity].to_i * product.price
-    calc_tax = calc_subtotal * 0.09
-    calc_total = calc_subtotal + calc_tax
+      calc_subtotal = params[:quantity].to_i * product.price
+      calc_tax = calc_subtotal * 0.09
+      calc_total = calc_subtotal + calc_tax
 
     @order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total]
+      subtotal: calc_subtotal,
+      tax: calc_tax,
+      total: calc_total
     )
 
     @order.save
