2d6d1075c97cc6fc8ec4c94f8ae2e390cf8ab31f
Kimber Ogden <mx.kimber@gmail.com>
2023-04-18 13:06:29 -0500

    make orders create actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,11 @@ class OrdersController < ApplicationController
   end
 
   def create
+    product = Product.find_by(id: params[:product_id])
+    calc_subtotal = params[:quantity].to_i * product.price
+    calc_tax = calc_subtotal * 0.09
+    calc_total = calc_subtotal + calc_tax
+
     @order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
